a7b01891a0b53ce31db7bca8d5eb1753396f1500
C.Zhu <zettosyuu@gmail.com>
2023-07-09 23:04:31 +0200

    add vk_mem_alloc

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Vulkan_Experiment_01
 Nothing but a TRY
+# Requirement
+CMake: 3.16~
--- src/Vulkan_Experiment_01.cpp
+++ src/Vulkan_Experiment_01.cpp
@@ -1,7 +1,4 @@
-﻿// Vulkan_Experiment_01.cpp: 定义应用程序的入口点。
-//
-
-#include "Vulkan_Experiment_01.h"
+﻿#include "Vulkan_Experiment_01.h"
 
 using namespace std;
 
